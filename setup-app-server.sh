f0ed68e0b4267896a1b345d1759f0151109988c7
facto974 <123896139+facto974@users.noreply.github.com>
2025-11-27 09:49:27 +0100

    Mettre à jour setup-app-server.sh avec clonage et création de dossier

--- setup-app-server.sh
+++ setup-app-server.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "Création du dossier projet..."
+mkdir -p TopGainersCryptoProject
+cd TopGainersCryptoProject
+
+echo "Clonage du dépôt GitHub..."
+git clone https://oauth2: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@github.com/romrec/TopGainersCryptoProject.git .
+
 echo "Installation des dépendances..."
 pip install --upgrade pip
 pip install streamlit requests
